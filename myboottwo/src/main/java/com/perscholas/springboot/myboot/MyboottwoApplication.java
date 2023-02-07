7e283b746a8313cfff4e148b1b8c84a2917fed5b
M_N <23023492+Malreon@users.noreply.github.com>
2023-02-07 14:26:12 -0600

    Add files via upload

---
+++ myboottwo/src/main/java/com/perscholas/springboot/myboot/MyboottwoApplication.java
@@ -0,0 +1,13 @@
+package com.perscholas.springboot.myboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyboottwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyboottwoApplication.class, args);
+	}
+
+}
---
+++ myboottwo/src/test/java/com/perscholas/springboot/myboot/MyboottwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perscholas.springboot.myboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyboottwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
