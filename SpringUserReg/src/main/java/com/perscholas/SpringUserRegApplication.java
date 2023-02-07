debf58490d50a55fb262c2ab807c0ee2530b3242
M_N <23023492+Malreon@users.noreply.github.com>
2023-02-07 14:23:26 -0600

    Add files via upload

---
+++ SpringUserReg/src/main/java/com/perscholas/SpringUserRegApplication.java
@@ -0,0 +1,13 @@
+package com.perscholas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUserRegApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUserRegApplication.class, args);
+	}
+
+}
